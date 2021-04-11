4a0aabb0123a489ac1edd078fd02bd660580523e
gloomysw <sw.gloomysw@gmail.com>
2021-04-11 19:14:57 +0800

    ✨ 增加第一页数据获取及渲染

--- view/src/page/home/index.jsx
+++ view/src/page/home/index.jsx
@@ -2,10 +2,27 @@ import React from 'react';
 import './index.pcss'
 import style from './index.pcss.json'
 import Menu from "../../components/menu";
+import axios from "axios";
 
 class Home extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            list: []
+        }
+    }
+
+    componentDidMount() {
+        this.wallpaperGetImgList()
+    }
+
+    wallpaperGetImgList = () => {
+        axios.get("http://127.0.0.1:8082/wallHavenTopList?pageIndex=1").then(res => {
+            const {data: {code, data}} = res
+            if (code === 200) {
+                this.setState({list: data})
+            }
+        })
     }
 
     render() {
@@ -14,56 +31,17 @@ class Home extends React.Component {
                 <Menu/>
                 <div className={style.contentArea}>
                     <ul>
-                        <li className={style.imgArea}>
-                            <figure className={style.picture}>
-                                <div className={style.img}></div>
-                            </figure>
-                        </li>
-                        <li className={style.imgArea}>
-                            <figure className={style.picture}>
-                                <div className={style.img}></div>
-                            </figure>
-                        </li>
-                        <li className={style.imgArea}>
-                            <figure className={style.picture}>
-                                <div className={style.img}></div>
-                            </figure>
-                        </li>
-                        <li className={style.imgArea}>
-                            <figure className={style.picture}>
-                                <div className={style.img}></div>
-                            </figure>
-                        </li>
-                        <li className={style.imgArea}>
-                            <figure className={style.picture}>
-                                <div className={style.img}></div>
-                            </figure>
-                        </li>
-                        <li className={style.imgArea}>
-                            <figure className={style.picture}>
-                                <div className={style.img}></div>
-                            </figure>
-                        </li>
-                        <li className={style.imgArea}>
-                            <figure className={style.picture}>
-                                <div className={style.img}></div>
-                            </figure>
-                        </li>
-                        <li className={style.imgArea}>
-                            <figure className={style.picture}>
-                                <div className={style.img}></div>
-                            </figure>
-                        </li>
-                        <li className={style.imgArea}>
-                            <figure className={style.picture}>
-                                <div className={style.img}></div>
-                            </figure>
-                        </li>
-                        <li className={style.imgArea}>
-                            <figure className={style.picture}>
-                                <div className={style.img}></div>
-                            </figure>
-                        </li>
+                        {
+                            this.state.list.map((item, index) =>
+                                <li className={style.imgArea} key={index}>
+                                    <figure className={style.picture}>
+                                        <div className={style.img}>
+                                            <img src={item.img_url} alt=""/>
+                                        </div>
+                                    </figure>
+                                </li>
+                            )
+                        }
                     </ul>
                 </div>
             </>
